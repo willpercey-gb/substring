5072a7741f25d8bb51ff801d62b7cd0d0b95b6ed
Will Percey <33656628+willpercey-gb@users.noreply.github.com>
2020-02-11 18:49:29 +0000

    Multibyte Substring function

---
+++ src.php
@@ -0,0 +1,15 @@
+ protected function substring($document, $start = false, $end = false)
+    {
+        if ($start) {
+            $start_position = mb_stripos($document, $start);
+            if (!($start_position === false)) $start_position = $start_position + strlen($start);
+        } else $start_position = 0;
+
+        if ($end) $end_position = @mb_stripos($document, $end, $start_position);
+        else {
+            $end_position = strlen($document);
+        }
+        if ($start_position === false || $end_position === false) return false;
+        return trim(substr($document, $start_position, $end_position - $start_position));
+
+    }
